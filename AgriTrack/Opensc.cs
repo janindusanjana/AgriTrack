eb0d3c60a818492802b4e5a696e23053df502755
Niroshan Maduranga <niroshanliyanagamage234@gmail.com>
2026-06-11 10:46:53 +0530

    Added User and Admin login panels

--- AgriTrack/Opensc.cs
+++ AgriTrack/Opensc.cs
@@ -44,5 +44,57 @@ namespace AgriTrack
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlWorkerLogin_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnWorker_Click_1(object sender, EventArgs e)
+        {
+            pnlWorker.Visible = false;      // පරණ එක හංගනවා
+            pnlWorkerLogin.Visible = true;  // ලොගින් එක පෙන්නනවා
+            pnlWorkerLogin.BringToFront();  // ලොගින් එක ඉස්සරහටම ගන්නවා
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pnlWorkerLogin.Visible = false; // ලොගින් එක හංගනවා
+            pnlWorker.Visible = true;       // පරණ එක ආයේ පෙන්නනවා
+        }
+
+        private void lblChoose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlBody_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Opensc_Load(object sender, EventArgs e)
+        {
+            pnlWorkerLogin.Visible = false;
+            pnlAdminLogin.Visible = false;
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            pnlAdmin.Visible = false;      // පරණ Admin එක හංගනවා
+            pnlAdminLogin.Visible = true;  // Admin ලොගින් එක පෙන්නනවා
+            pnlAdminLogin.BringToFront();  // ඒක ඉස්සරහටම ගන්නවා
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pnlAdminLogin.Visible = false; // ලොගින් එක හංගනවා
+            pnlAdmin.Visible = true;       // පරණ එක ආයේ පෙන්නනවා
+        }
     }
 }
--- AgriTrack/Program.cs
+++ AgriTrack/Program.cs
@@ -13,7 +13,7 @@ namespace AgriTrack
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new dashboard1());
+            Application.Run(new Opensc());
         }
     }
 }

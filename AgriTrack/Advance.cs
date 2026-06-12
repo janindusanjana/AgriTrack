bbae03694b4d11fbe4f133de2a2926fbdf1066a2
NeranjaLakshan <gnlneranjalakshan@gmail.com>
2026-06-12 12:54:42 +0530

    Advance Ui created
    hy there

---
+++ AgriTrack/Advance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgriTrack
+{
+    public partial class Advance : Form
+    {
+        public Advance()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

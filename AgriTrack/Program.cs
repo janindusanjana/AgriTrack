f040dbad121cbe83e6de1b9977978112a298768f
janindusanjana <janindusanjanagallage@gmail.com>
2026-06-15 21:59:28 +0530

    Daily Harvest_Form_admin shows crud operations have done

--- AgriTrack/Program.cs
+++ AgriTrack/Program.cs
@@ -13,7 +13,7 @@ namespace AgriTrack
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new add_farmmer_1());
+            Application.Run(new Daily_Harvest());
         }
     }
 }
